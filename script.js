705cc72b7c0be4b70868451ac5253a654db2145c
Lara Filippone <lara.filippone@gmail.com>
2022-03-16 09:22:29 +0100

    function that create a newCell this function create a new element 'div' in html

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 /*
 creare una griglia di gioco quadrata, in cui ogni cella contiene un numero tra quelli compresi in un range compreso tra 1 e 100
 Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.
+
 BONUS:
 L'utente indica un livello di difficoltà, in base al livello scelto la griglia conterrà un range diverso:
 con difficoltà 1 => tra 1 e 100
@@ -14,8 +15,14 @@ console.log('JS ok');
 // Creo grigllia con js
 const square = document.getElementById('square');
 
-// Creo  numero  righe e colonne e calcollo numeero di celle
+// Creo  numero  righe e colonne e calcolo numeero di celle
 const row = 10;
 const column = 10;
 const totCell = row * column;
 
+// Creo funzione che crea un div per la cella
+function newCell{
+    const cell = document.createElement('div');
+    cell.classList.add('cell');
+    return cell;
+}
